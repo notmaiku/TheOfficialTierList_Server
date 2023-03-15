c965b1fc6d710fbdb6af47cd1e12b279483d56e2
notMaiku <mike1davis@rocketmail.com>
2023-03-15 02:45:41 -0400

    cors

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,16 @@ use sea_orm::Database;
 use sea_orm::ConnectOptions;
 use tokio::time::Duration;
 use std::net::SocketAddr;
-use crate::routes::create_routes;
+use http::{Request, Response, Method, header};
+use tower_http::cors::{Any, CorsLayer};
 
 pub async fn run(database_uri: &str){
+    let cors = CorsLayer::new()
+    // allow `GET` and `POST` when accessing the resource
+    .allow_methods([Method::GET, Method::POST])
+    // allow requests from any origin
+    .allow_origin(Any);
+
     let database_url = database_uri.to_owned();
     let db = match Database::connect(database_url).await {
         Ok(db) => db,
@@ -16,10 +23,11 @@ pub async fn run(database_uri: &str){
             panic!();
         }
     };
-    let app = routes::create_routes(db);
+    let app = routes::create_routes(db).layer(cors);
     let addr = SocketAddr::from(([0,0,0,0], 3000));
-    println!("listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await;
+        .await
+        .unwrap();
 }
