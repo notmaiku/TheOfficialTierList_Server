d2d82bce5ee6d77a10376aea0500b8bd82dc1aeb
notmaiku <mike1davis@rocketmail.com>
2025-02-10 15:45:42 -0500

    port change

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@ pub async fn run() {
 
     println!("Playground: http://localhost:3000/api/graphql");
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:38228".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
